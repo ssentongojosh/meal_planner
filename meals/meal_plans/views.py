2e0bc5aaf5cff705f280fe325438ebebb936f255
ssentongoJoshua <ssentongoj05hu4@gmail.com>
2025-02-24 14:03:54 +0300

    activated app by adding it to installed apps list in settings.py file

--- meals/meal_plans/views.py
+++ meals/meal_plans/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 """Home page view"""
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'meal_plans/home.html')
